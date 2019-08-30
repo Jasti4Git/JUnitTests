c74ea606bf1d8dd264a200febfbe07168ad0febc
Jasti4git <sreemanthjasti91@gmail.com>
2019-08-29 20:34:18 -0400

    Adding beforEeach and afterEach annotations and methods.

--- JUnitTests/src/test/java/samples/MathUtilsTest.java
+++ JUnitTests/src/test/java/samples/MathUtilsTest.java
@@ -2,24 +2,39 @@ package samples;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
+	
+	MathUtils utils;
+	
+	@BeforeEach
+	void init() {
+		
+		 utils=new MathUtils();
+
+	}
 
 	@Test
 	void testAddition() {
 		int expected=4;
-		MathUtils addition=new MathUtils();
-		int actual=addition.addition(1, 3);
+		int actual=utils.addition(1, 3);
 		assertEquals(expected,actual,"add properly");
 	
 	}
 
 	@Test
 	void testDivide() {
-		MathUtils division=new MathUtils();
-		assertThrows(ArithmeticException.class, ()->division.divide(1, 1));
+		assertThrows(ArithmeticException.class, ()->utils.divide(1, 1));
 	
+	}
+	
+	@AfterEach
+	void clear()
+	{
+		System.gc();
 	}
 	
 }
