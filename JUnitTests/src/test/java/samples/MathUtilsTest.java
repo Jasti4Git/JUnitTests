b04b66b539f84e81b4d02163e380e8e5e1350453
Jasti4git <sreemanthjasti91@gmail.com>
2019-09-03 20:34:02 -0400

    "Adding a nested test"

--- JUnitTests/src/test/java/samples/MathUtilsTest.java
+++ JUnitTests/src/test/java/samples/MathUtilsTest.java
@@ -2,54 +2,78 @@ package samples;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
 
 class MathUtilsTest {
-	
+
 	MathUtils utils;
-	
-	
+
 	@BeforeAll
-	static void beforeInit()
-	{
+	static void beforeInit() {
 		System.out.println("in before init method");
-		
+
 	}
+
 	@BeforeEach
 	void init() {
-		
-		 utils=new MathUtils();
+
+		utils = new MathUtils();
 
 	}
 
-	@Test
-	void testAddition() {
-		int expected=4;
-		int actual=utils.addition(1, 3);
-		assertEquals(expected,actual,"add properly");
-	
+	@Nested
+	class addtest {
+
+		@Test
+		@DisplayName("Testing Add Method with positives")
+		void testAdditionwithpositives() {
+			int expected = 4;
+			int actual = utils.addition(1, 3);
+			assertEquals(expected, actual, "add properly");
+
+		}
+
+		@Test
+		@DisplayName("Testing Add Method with negatives")
+		void testAdditionwithNegatives() {
+			int expected = -4;
+			int actual = utils.addition(-1, -3);
+			assertEquals(-4, actual, "add properly");
+
+		}
+
 	}
 
 	@Test
 	void testDivide() {
-		assertThrows(ArithmeticException.class, ()->utils.divide(1, 1));
-	
+		assertThrows(ArithmeticException.class, () -> utils.divide(1, 0));
+
 	}
-	
+
+	@Test
+	@EnabledOnJre(JRE.JAVA_8)
+	void testForJava() {
+		System.out.println("Java 8 testcase");
+
+	}
+
 	@AfterEach
-	void clear()
-	{
+	void clear() {
 		System.gc();
 	}
-	
-	@BeforeAll
-	static void afterInit()
-	{
+
+	@AfterAll
+	static void afterInit() {
 		System.out.println("in after init method");
-		
+
 	}
-	
+
 }
